6b16eff3e4016dc4a3d1fd05d4247a905ffee2e6
ShahilShadad <sshahilshadadislam@gmail.com>
2025-11-04 18:25:18 +0100

    commit_Shahil_1

--- Empleado.php
+++ Empleado.php
@@ -10,26 +10,5 @@
             $this->apellidos= $apellidos;
             $this->salarioBase = $salarioBase;
         }
-        //Esto lo hace Shahil
-        function calcularSalarioAnual(){
-        }
-        function devolverNombreCompleto(){
-        }
-    }
-    class Gerente extends Empleado{
-        public $bonoGerente = 10;
-        public function __construct($nombre,$apellidos,$salarioBase)
-        {
-        parent::__construct($nombre,$apellidos,$salarioBase);
-
-        }
-    }
-    class Vendedor extends Empleado{
-        public $bonoEmpleado;
-        public function __construct($nombre,$apellidos,$salarioBase)
-        {
-        parent::__construct($nombre,$apellidos,$salarioBase);
-        $this->bonoEmpleado = rand(0,10);
-        }
     }
 ?>
