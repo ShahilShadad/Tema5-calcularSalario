6afcbfbc05a61ced3d0017c88fa20c00229b2813
ShahilShadad <sshahilshadadislam@gmail.com>
2025-11-10 12:29:16 +0100

    commit develop index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    //Ruben hace que salga el nombre del usuario con un saludo
+
+    //Esto lo hace Shahil (Como se añade empleados y salario base al pulsar el botón)
+    require_once "Empleado.php";
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular Salarios</title>
+</head>
+<body>
+    <!--Saludo con nombre del Usuario (Ruben)--> 
+    <!--Formulario con inputs y boton calcular salario-->
+</body>
+</html>
